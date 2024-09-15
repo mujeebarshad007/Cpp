0b63da029e40339d806b2da1ec570cac8f131ba9
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2024-09-15 20:43:16 +0500

    Sum

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    cout << "Hello world \n";
-    return 0;
-}
+int main(){
+    int a=6;
+    int b=7;
+    int sum;
+    sum = a+b;
+    cout << "The sum of "<<a<<" and "<<b<<" is "<<13<<"\n";
+return 0;}
