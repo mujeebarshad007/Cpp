1d4561f20ab9f8eac28d276dcda538efc926de7d
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2024-09-27 17:01:31 +0500

    Your commit message

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a=6;
-    int b=7;
-    int sum;
-    sum = a+b;
-    cout << "The sum of "<<a<<" and "<<b<<" is "<<13<<"\n";
-return 0;}
