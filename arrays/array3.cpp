5cac37e3fda3020f62366a9739e5c22925e4655f
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2024-12-08 16:26:16 +0500

    New

--- arrays/array3.cpp
+++ arrays/array3.cpp
@@ -17,18 +17,21 @@ int main()
     int size = 9;
     int smallest = INT_MAX;
     int largest = INT_MIN;
+    int smallestindex = -1, largestindex = -1;
     for (int i = 0; i < size; i++)
     {
         if (num[i] < smallest)
         {
-            smallest = i;
+            smallest = num[i];
+            smallestindex = i;
         }
         if (num[i] > largest)
         {
             largest = num[i];
+            largestindex = i;
         }
     }
-    cout << " The Smallest In this array is at = " << i << endl;
-    cout << " The Largest In this array is at= " << largest << endl;
+    cout << " The Smallest In this array is at index " << smallestindex << " and is " << smallest << endl;
+    cout << " The Largest In this array is at index  " << largestindex << " and is" << largest << endl;
     return 0;
 }
